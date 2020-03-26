055e246ae344fb36171907b6dab42469c9768b86
mbarahonalopez <62214674+mbarahonalopez@users.noreply.github.com>
2020-03-26 17:27:55 +0100

    Add files via upload

--- WindTurbine/MAIN_BaselineResults.py
+++ WindTurbine/MAIN_BaselineResults.py
@@ -7,10 +7,11 @@ import BEMmodel as BEM
 N = 100 # annulus spacing
 mode = 'constant' # 2 possible modes: constant and cosinus distribution of annulus. 
                   # Write 'cosinus' or 'constant' if you want to change the distribution.
-plotter = False # If you don't want to plot all the default plots please enter False
-TSR = [6, 8, 10]
-[results, CT, CP, Uinf, Radius] = BEM.ExecuteBEM(N, plotter, mode, TSR[0])
+plotter = True # If you don't want to plot all the default plots please enter False
 
+TSR = [6, 8, 10]
+
+[results, CT, CP, Uinf, Radius] = BEM.ExecuteBEM(N, plotter, mode, TSR[0])
 alpha = np.zeros([len(results[:,0]),3])
 a = np.zeros([len(results[:,0]),3])
 ap = np.zeros([len(results[:,0]),3])
@@ -18,6 +19,7 @@ phi = np.zeros([len(results[:,0]),3])
 Ct = np.zeros([len(results[:,0]),3])
 Cn = np.zeros([len(results[:,0]),3])
 Cq = np.zeros([len(results[:,0]),3])
+Circ = np.zeros([len(results[:,0]),3])
 
 for i in range(0,3):
     [results, CT, CP, Uinf, Radius] = BEM.ExecuteBEM(N, plotter, mode, TSR[i])
@@ -27,7 +29,8 @@ for i in range(0,3):
     ap[:,i] = results[:,1]
     Ct[:,i] = results[:,4]
     Cn[:,i] = results[:,3]
-    Cq[:,i] = results[:,8]    
+    Cq[:,i] = results[:,8] 
+    Circ[:,i] = results[:,5]   
 
 """ Plots Section II.C """
 
@@ -37,8 +40,10 @@ plt.plot(results[:,2], alpha[:,0], 'r-', label=r'$\lambda=6$')
 plt.plot(results[:,2], alpha[:,1], 'b-', label=r'$\lambda=8$')
 plt.plot(results[:,2], alpha[:,2], 'g-', label=r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
-plt.ylabel(r'$\alpha$', fontsize=20)
+plt.ylabel(r'$\alpha$ [deg]', fontsize=20)
 plt.legend(fontsize=20)
 plt.savefig('Alpha_WT_Res.pdf', format='pdf', dpi=1000)
 
@@ -48,8 +53,10 @@ plt.plot(results[:,2], phi[:,0], 'r-', label=r'$\lambda=6$')
 plt.plot(results[:,2], phi[:,1], 'b-', label=r'$\lambda=8$')
 plt.plot(results[:,2], phi[:,2], 'g-', label=r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
-plt.ylabel(r'$\phi$', fontsize=20)
+plt.ylabel(r'$\phi$ [deg]', fontsize=20)
 plt.legend(fontsize=20)
 plt.savefig('phi_WT_Res.pdf', format='pdf', dpi=1000)
 
@@ -60,6 +67,8 @@ plt.plot(results[:,2], a[:,0], 'r-', label=r'$\lambda=6$')
 plt.plot(results[:,2], a[:,1], 'b-', label=r'$\lambda=8$')
 plt.plot(results[:,2], a[:,2], 'g-', label=r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
 plt.ylabel(r'$a$', fontsize=20)
 plt.legend(fontsize=20)
@@ -71,6 +80,8 @@ plt.plot(results[:,2], ap[:,0], 'r-', label=r'$\lambda=6$')
 plt.plot(results[:,2], ap[:,1], 'b-', label=r'$\lambda=8$')
 plt.plot(results[:,2], ap[:,2], 'g-', label=r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
 plt.ylabel(r'$a^,$', fontsize=20)
 plt.legend(fontsize=20)
@@ -78,10 +89,12 @@ plt.savefig('ap_WT_Res.pdf', format='pdf', dpi=1000)
 
 # Cn vs. r/R
 fig1 = plt.figure(figsize=(12, 6))
-plt.plot(results[:,2], Cn[:,0], 'r-', label=r'$\lambda=6$')
-plt.plot(results[:,2], Cn[:,1], 'b-', label=r'$\lambda=8$')
-plt.plot(results[:,2], Cn[:,2], 'g-', label=r'$\lambda=10$')
+plt.plot(results[:,2], Cn[:,0]/(0.5*Uinf**2*Radius), 'r-', label=r'$\lambda=6$')
+plt.plot(results[:,2], Cn[:,1]/(0.5*Uinf**2*Radius), 'b-', label=r'$\lambda=8$')
+plt.plot(results[:,2], Cn[:,2]/(0.5*Uinf**2*Radius), 'g-', label=r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
 plt.ylabel(r'$C_n$', fontsize=20)
 plt.legend(fontsize=20)
@@ -89,10 +102,12 @@ plt.savefig('Cn_WT_Res.pdf', format='pdf', dpi=1000)
 
 # Ct vs. r/R
 fig1 = plt.figure(figsize=(12, 6))
-plt.plot(results[:,2], Ct[:,0], 'r-', label=r'$\lambda=6$')
-plt.plot(results[:,2], Ct[:,1], 'b-', label=r'$\lambda=8$')
-plt.plot(results[:,2], Ct[:,2], 'g-', label=r'$\lambda=10$')
+plt.plot(results[:,2], Ct[:,0]/(0.5*Uinf**2*Radius), 'r-', label=r'$\lambda=6$')
+plt.plot(results[:,2], Ct[:,1]/(0.5*Uinf**2*Radius), 'b-', label=r'$\lambda=8$')
+plt.plot(results[:,2], Ct[:,2]/(0.5*Uinf**2*Radius), 'g-', label=r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
 plt.ylabel(r'$C_t$', fontsize=20)
 plt.legend(fontsize=20)
@@ -100,11 +115,27 @@ plt.savefig('Ct_WT_Res.pdf', format='pdf', dpi=1000)
 
 # Cn vs. r/R
 fig1 = plt.figure(figsize=(12, 6))
-plt.plot(results[:,2], Cq[:,0], 'r-', label=r'$\lambda=6$')
-plt.plot(results[:,2], Cq[:,1], 'b-', label=r'$\lambda=8$')
-plt.plot(results[:,2], Cq[:,2], 'g-', label=r'$\lambda=10$')
+plt.plot(results[:,2], Cq[:,0]/(0.5*Uinf**2*Radius**2), 'r-', label=r'$\lambda=6$')
+plt.plot(results[:,2], Cq[:,1]/(0.5*Uinf**2*Radius**2), 'b-', label=r'$\lambda=8$')
+plt.plot(results[:,2], Cq[:,2]/(0.5*Uinf**2*Radius**2), 'g-', label=r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
 plt.ylabel(r'$C_q$', fontsize=20)
 plt.legend(fontsize=20)
 plt.savefig('Cq_WT_Res.pdf', format='pdf', dpi=1000)
+
+# Circulation vs. r/R
+NBlades = 3
+fig1 = plt.figure(figsize=(12, 6))
+plt.plot(results[:,2], Circ[:,0]/(np.pi*Uinf**2/(NBlades*Uinf*TSR[0]/Radius)), 'r-', label=r'$\lambda=6$')
+plt.plot(results[:,2], Circ[:,1]/(np.pi*Uinf**2/(NBlades*Uinf*TSR[1]/Radius)), 'b-', label=r'$\lambda=8$')
+plt.plot(results[:,2], Circ[:,2]/(np.pi*Uinf**2/(NBlades*Uinf*TSR[2]/Radius)), 'g-', label=r'$\lambda=10$')
+plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
+plt.xlabel('r/R', fontsize=20)
+plt.ylabel(r'$\Gamma$', fontsize=20)
+plt.legend(fontsize=20)
+plt.savefig('Gamma_WT_Res.pdf', format='pdf', dpi=1000)
--- WindTurbine/MAIN_Opt.py
+++ WindTurbine/MAIN_Opt.py
@@ -40,13 +40,16 @@ aline = np.zeros([len(r_R),3])
 vmag2 = np.zeros([len(r_R),3])
 CP = np.zeros(3)
 CT = np.zeros(3)
-while (finish==False):
     
-    for i in range(0,3):
+for i in range(0,3):
+    
+    finish = False
+    while (finish==False):
+        
         Variables = [CT_new, NB, Radius, Uinf, TSR[i]]
         [CP[i], CT[i], fnorm[:,i], ftan[:,i], gamma[:,i], chord[:,i], twist[:,i], r_R, phi[:,i], a[:,i], aline[:,i], vmag2[:,i]] = BEM_Opt.ExecuteBEM_opt(N, Variables)
 
-        if (abs(CT0-CT[i])<0.001):
+        if (abs(CT0-CT[i])<0.0001):
             
             finish = True
             
@@ -58,11 +61,12 @@ while (finish==False):
 
 # Chord distribution
 fig1 = plt.figure(figsize=(12, 6))
-plt.title(r'Chord distribution in Propeller', fontsize=20)
 plt.plot(r_R, chord[:,0], 'r-', label = r'$\lambda=6$')
 plt.plot(r_R, chord[:,1], 'b-', label = r'$\lambda=8$')
 plt.plot(r_R, chord[:,2], 'g-', label = r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
 plt.ylabel(r'$c(r/R)$ [m]', fontsize=20)
 plt.legend(fontsize=20)
@@ -70,11 +74,12 @@ plt.savefig('Chord_distrib_WT.pdf', format='pdf', dpi=1000)
 
 # Twist distribution
 fig1 = plt.figure(figsize=(12, 6))
-plt.title(r'Twist distribution in Propeller', fontsize=20)
 plt.plot(r_R, twist[:,0], 'r-',  label = r'$\lambda=6$')
 plt.plot(r_R, twist[:,1], 'b-', label = r'$\lambda=8$')
 plt.plot(r_R, twist[:,2], 'g-', label = r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
 plt.ylabel(r'$\theta(r/R)$ [deg]', fontsize=20)
 plt.legend(fontsize=20)
@@ -82,49 +87,53 @@ plt.savefig('Twist_distrib_WT.pdf', format='pdf', dpi=1000)
 
 # Fnorm distribution
 fig1 = plt.figure(figsize=(12, 6))
-plt.title(r'Normal force distribution in Propeller', fontsize=20)
-plt.plot(r_R, fnorm[:,0], 'r-',  label = r'$\lambda=6$')
-plt.plot(r_R, fnorm[:,1], 'b-', label = r'$\lambda=8$')
-plt.plot(r_R, fnorm[:,2], 'g-', label = r'$\lambda=10$')
+plt.plot(r_R, fnorm[:,0]/(0.5*Uinf**2*Radius), 'r-',  label = r'$\lambda=6$')
+plt.plot(r_R, fnorm[:,1]/(0.5*Uinf**2*Radius), 'b-', label = r'$\lambda=8$')
+plt.plot(r_R, fnorm[:,2]/(0.5*Uinf**2*Radius), 'g-', label = r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
-plt.ylabel(r'$f_n(r/R)$ [N]', fontsize=20)
+plt.ylabel(r'$C_n(r/R)$ ', fontsize=20)
 plt.savefig('fnorm_distrib_WT.pdf', format='pdf', dpi=1000)
 plt.legend(fontsize=20)
 
 # Circulation distribution
 fig1 = plt.figure(figsize=(12, 6))
-plt.title(r'Circulation distribution in Propeller', fontsize=20)
-plt.plot(r_R, gamma[:,0], 'r-', label = r'$\lambda=6$')
-plt.plot(r_R, gamma[:,1], 'b-', label = r'$\lambda=8$')
-plt.plot(r_R, gamma[:,2], 'g-', label = r'$\lambda=10$')
+plt.plot(r_R, gamma[:,0]/(np.pi*Uinf**2/(NB*Uinf*TSR[0]/Radius)), 'r-', label = r'$\lambda=6$')
+plt.plot(r_R, gamma[:,1]/(np.pi*Uinf**2/(NB*Uinf*TSR[0]/Radius)), 'b-', label = r'$\lambda=8$')
+plt.plot(r_R, gamma[:,2]/(np.pi*Uinf**2/(NB*Uinf*TSR[0]/Radius)), 'g-', label = r'$\lambda=10$')
 plt.xlabel('r/R', fontsize=20)
 plt.grid()
-plt.ylabel(r'$\Gamma(r/R)$ [1/s]', fontsize=20)
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
+plt.ylabel(r'$\Gamma(r/R)$', fontsize=20)
 plt.legend(fontsize=20)
 plt.savefig('Gamma_WT.pdf', format='pdf', dpi=1000)
 
 # Inflow angle distribution
 fig1 = plt.figure(figsize=(12, 6))
-plt.title(r'Inflow distribution in Propeller', fontsize=20)
 plt.plot(r_R, phi[:,0], 'r-', label = r'$\lambda=6$')
 plt.plot(r_R, phi[:,1], 'b-', label = r'$\lambda=8$')
 plt.plot(r_R, phi[:,2], 'g-', label = r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
-plt.ylabel(r'$\phi(r/R)$ [1/s]', fontsize=20)
+plt.ylabel(r'$\phi(r/R)$ [deg]', fontsize=20)
 plt.legend(fontsize=20)
 plt.savefig('phi_WT.pdf', format='pdf', dpi=1000)
 
 # Inflow angle distribution
 fig1 = plt.figure(figsize=(12, 6))
-plt.title(r'Inflow distribution in Propeller', fontsize=20)
 plt.plot(r_R, a[:,0], 'r-', label = r'$\lambda=6$')
 plt.plot(r_R, a[:,1], 'b-', label = r'$\lambda=8$')
 plt.plot(r_R, a[:,2], 'g-', label = r'$\lambda=10$')
 plt.grid()
+plt.tick_params(axis='x', which='major', labelsize=20)
+plt.tick_params(axis='y', which='major', labelsize=20)
 plt.xlabel('r/R', fontsize=20)
-plt.ylabel(r'$a(r/R)$ [1/s]', fontsize=20)
+plt.ylabel(r'$a(r/R)$ ', fontsize=20)
 plt.legend(fontsize=20)
 plt.savefig('a_WT.pdf', format='pdf', dpi=1000)
 
